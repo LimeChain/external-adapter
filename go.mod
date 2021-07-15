a9244d2feb37ad223aaa848feba43dff5e401f76
Rusi Boayadjiev <rusi.boyadjiev@jboxers.com>
2021-07-15 11:52:58 +0300

    Init Hedera's external adapter

---
+++ go.mod
@@ -0,0 +1,9 @@
+module main.go
+
+go 1.16
+
+require (
+	github.com/hashgraph/hedera-sdk-go v0.9.4
+	github.com/hashgraph/hedera-sdk-go/v2 v2.1.10 // indirect
+	github.com/joho/godotenv v1.3.0
+)
---
+++ main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/hashgraph/hedera-sdk-go/v2"
+	"github.com/joho/godotenv"
+)
+
+type JobResult struct {
+	Id   string
+	Data DataJson
+}
+
+type DataJson struct {
+	Result        string
+	HederaTopicId string
+}
+
+func externalAdapterHandler(res http.ResponseWriter, req *http.Request) {
+
+	log.Println("uurl: ", req.URL)
+
+	if req.URL.Path != "/" {
+		http.Error(res, "404 not found.", http.StatusNotFound)
+		return
+	}
+
+	if req.Method != http.MethodPost {
+		http.Error(res, "405 Method not allowed.", http.StatusMethodNotAllowed)
+		return
+	}
+
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(req.Body)
+
+	var jobResult JobResult
+	json.Unmarshal(buf.Bytes(), &jobResult)
+
+	transactionReceipt := submitMessageToTopic(jobResult.Data.HederaTopicId, buf.Bytes())
+
+	fmt.Fprintf(res, "{\"transactionStatus\": \"/%v\"}", transactionReceipt.Status)
+
+}
+
+func submitMessageToTopic (hederaTopicId string, message []byte) hedera.TransactionReceipt {
+	err := godotenv.Load(".env")
+	if err != nil {
+		panic(fmt.Errorf("Unable to load environment variables from .env file. Error:\n%v\n", err))
+	}
+
+	myAccountId, err := hedera.AccountIDFromString(os.Getenv("MY_ACCOUNT_ID"))
+	if err != nil {
+		panic(err)
+	}
+
+	myPrivateKey, err := hedera.PrivateKeyFromString(os.Getenv("MY_PRIVATE_KEY"))
+	if err != nil {
+		panic(err)
+	}
+
+	client := hedera.ClientForTestnet()
+	client.SetOperator(myAccountId, myPrivateKey)
+
+	topicId, err := hedera.TopicIDFromString(hederaTopicId)
+
+	if err != nil {
+		panic(err)
+	}
+
+	transaction := hedera.NewTopicMessageSubmitTransaction().
+		SetTopicID(topicId).
+		SetMessage(message)
+
+	txResponse, err := transaction.Execute(client)
+	if err != nil {
+		panic(err)
+	}
+
+	transactionReceipt, err := txResponse.GetReceipt(client)
+	if err != nil {
+		panic(err)
+	}
+
+	return transactionReceipt
+}
+
+
+func main() {
+
+	http.HandleFunc("/", externalAdapterHandler)
+
+	http.ListenAndServe(":8090", nil)
+}
